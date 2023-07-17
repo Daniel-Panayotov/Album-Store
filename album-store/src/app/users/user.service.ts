6cc0e8165c85a6aa16523afa46e8fa34d67b4a8f
DoggoFromWater <116671341+DoggoFromWater@users.noreply.github.com>
2023-07-17 22:25:25 +0300

    login func

--- album-store/src/app/users/user.service.ts
+++ album-store/src/app/users/user.service.ts
@@ -3,8 +3,10 @@ import {
   Firestore,
   collection,
   collectionData,
-  doc,
   addDoc,
+  setDoc,
+  query,
+  where,
 } from '@angular/fire/firestore';
 import { User } from '../types/user';
 
@@ -15,16 +17,35 @@ export class UserService {
   isLoggedIn: boolean = false;
 
   constructor(private fs: Firestore) {
-    this.checkUser();
+    this.findUser('abdull_100@abv.bg').subscribe((v) => console.log(v));
   }
 
   registerUser(userData: User): Promise<any> {
     return addDoc(collection(this.fs, 'users'), userData);
   }
 
-  checkUser(): void {
-    collectionData(collection(this.fs, 'users'), { idField: 'id' }).subscribe(
-      (users) => console.log('Stinky!')
+  loginUser(userData: User) {
+    const { email, password } = userData;
+
+    this.findUser(email).subscribe({
+      next(user) {
+        if (!user[0]) {
+          throw 'wrong email';
+        }
+        if (user[0]['password'] != password) {
+          throw 'wrong password';
+        }
+        //set cookie
+      },
+      error(err) {
+        console.log(err);
+      },
+    });
+  }
+
+  findUser(email: string) {
+    return collectionData(
+      query(collection(this.fs, 'users'), where('email', '==', email))
     );
   }
 }
