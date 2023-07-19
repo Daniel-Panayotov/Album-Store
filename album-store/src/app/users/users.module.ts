6d280b50df20c287be21565ba283144d6cebfa00
DoggoFromWater <116671341+DoggoFromWater@users.noreply.github.com>
2023-07-20 02:46:59 +0300

    del old routes

--- album-store/src/app/users/users.module.ts
+++ album-store/src/app/users/users.module.ts
@@ -2,25 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { FormsModule } from '@angular/forms';
-import { isLoggedActivate } from '../shared/guards/isLogged.activate';
-
-const routes: Routes = [
-  {
-    path: 'users',
-    children: [
-      { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent },
-      {
-        path: 'profile',
-        canActivate: [isLoggedActivate],
-        component: ProfileComponent,
-      },
-    ],
-  },
-];
 
 @NgModule({
   declarations: [RegisterComponent, LoginComponent, ProfileComponent],
