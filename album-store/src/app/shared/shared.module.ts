ac85643a310a9f94f1f8b1f5605980f8fb1e9cff
DoggoFromWater <116671341+DoggoFromWater@users.noreply.github.com>
2023-07-29 22:31:24 +0300

    remove unused component and interceptor

--- album-store/src/app/shared/shared.module.ts
+++ album-store/src/app/shared/shared.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoaderComponent } from './loader/loader.component';
 import { RouterModule } from '@angular/router';
 import { AlbumCardComponent } from './album-card/album-card.component';
 import { CommentComponent } from './comment/comment.component';
 
 @NgModule({
-  declarations: [LoaderComponent, AlbumCardComponent, CommentComponent],
+  declarations: [AlbumCardComponent, CommentComponent],
   imports: [CommonModule, RouterModule],
-  exports: [LoaderComponent, AlbumCardComponent, CommentComponent],
+  exports: [AlbumCardComponent, CommentComponent],
 })
 export class SharedModule {}
